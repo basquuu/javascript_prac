aee034fb9ff8b2c1ccee77bb17a5d6f0cc203a93
basquuu <111121556+basquuu@users.noreply.github.com>
2023-05-06 01:05:39 +0900

    [Chapter 3] #1 Css in JavaScript

--- momentum/script.js
+++ momentum/script.js
@@ -3,30 +3,12 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.dir(h1);
 
 function handleTitleClick(){
-    h1.style.color = "blue";
+    const currentColor = h1.style.color ; //변경 x
+    let newColor; //변경 o 
+    if (currentColor === "blue"){
+        newColor = "tomato";
+    }else{
+        newColor = "blue";
+    }h1.style.color = newColor;
 }
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
-function handleMouseLeave(){
-    h1.innerText ="Don't go mouse!";
-}
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy(){
-    alert("copier!");
-}
-function handleWindowOnline(){
-    alert("ALL GOOODDD");
-}
-function handleWindowOffline(){
-    alert("No wifi!");
-}
-h1.onclick =  handleTitleClick;
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave",handleMouseLeave);
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("online",handleWindowOnline);
-window.addEventListener("offline",handleWindowOffline);
+h1.addEventListener("click",handleTitleClick);
