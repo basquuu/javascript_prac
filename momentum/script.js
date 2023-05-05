1cd397e34e819c7f22b6b496086feb63fd33efe4
basquuu <111121556+basquuu@users.noreply.github.com>
2023-05-06 01:30:41 +0900

    [Chapter 3} #1 3.8 Css JS part 3

--- momentum/script.js
+++ momentum/script.js
@@ -4,10 +4,11 @@ console.dir(h1);
 
 function handleTitleClick(){
     const clickedClass = "clicked";
-    if(h1.className ===clickedClass){
-        h1.className = "";
+    /*if(h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass) = "";
     } else{
-        h1.className = clickedClass;
-    }
+        h1.classList.add(clickedClass);
+    }*/ 
+    h1.classList.toggle("clicked");// toggle라는 함수가 위 코드랑 동일한 기능을 함 
 }
 h1.addEventListener("click",handleTitleClick);
