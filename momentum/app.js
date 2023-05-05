1f0da764d6ec0c82207f97762cde86e9a8f4b307
basquuu <111121556+basquuu@users.noreply.github.com>
2023-05-06 02:54:01 +0900

    [Chapter 4] #2 4.3 Events part 2

--- momentum/app.js
+++ momentum/app.js
@@ -1,10 +1,14 @@
 const loginFrom = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     event.preventDefault(); //기본 브라우저 동작 막아줌 
     console.log(loginInput.value);
 }
-
+function handleLinkClick(event){
+    event.preventDefault();
+    //alert("clicked!");// 경고창 때문에 페이지 이동 막아줌 -> ok 누르면 이동
+}
 loginFrom.addEventListener("submit",onLoginSubmit);
-onLoginSubmit();
