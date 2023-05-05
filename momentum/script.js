ad7597a2715cee6cd16fd5b98cb0ddbb9c4fff63
basquuu <111121556+basquuu@users.noreply.github.com>
2023-05-06 00:26:00 +0900

    [chapter 3] #1 JS event

---
+++ momentum/script.js
@@ -0,0 +1,6 @@
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+    title.style.color = "blue";
+}
+title.addEventListener("click", handleTitleClick);
