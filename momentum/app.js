ddd2f8ebc7e85d9ce6f5632f5ac23c169c8859ca
basquuu <111121556+basquuu@users.noreply.github.com>
2023-05-06 03:46:16 +0900

    [Chapter 4]  #2 4.6 Loading Username

--- momentum/app.js
+++ momentum/app.js
@@ -1,16 +1,30 @@
-const loginFrom = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    loginFrom.classList.add(HIDDEN_CLASSNAME);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem('username',username);
+    localStorage.setItem(USERNAME_KEY,username);
+    painGreetings(username);
+}
+
+function paintGreeting(username){
     greeting.innerText = `Hello ${username}`; 
     greeting.classList.remove(HIDDEN_CLASSNAME);
-
 }
-loginFrom.addEventListener("submit",onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    //show the greeting;
+    paintGreeting(savedUsername)  
+}
