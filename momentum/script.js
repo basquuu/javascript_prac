65f5e0e2bf5d4f48b3c0c7ab87b33e895fd9eca6
basquuu <111121556+basquuu@users.noreply.github.com>
2023-05-06 01:17:27 +0900

    [chapter 3] #1 3.7 Css JS PART 2

--- momentum/script.js
+++ momentum/script.js
@@ -3,12 +3,11 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.dir(h1);
 
 function handleTitleClick(){
-    const currentColor = h1.style.color ; //변경 x
-    let newColor; //변경 o 
-    if (currentColor === "blue"){
-        newColor = "tomato";
-    }else{
-        newColor = "blue";
-    }h1.style.color = newColor;
+    const clickedClass = "clicked";
+    if(h1.className ===clickedClass){
+        h1.className = "";
+    } else{
+        h1.className = clickedClass;
+    }
 }
 h1.addEventListener("click",handleTitleClick);
